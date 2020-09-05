a9a0c48a2e62845eb33ae5c9e2d1571b068570db
vatsal <vatsaliit30@gmail.com>
2020-09-05 22:55:16 +0530

    modified: create_folds.py
    Updated mistake in creating Folds.

--- create_folds.py
+++ create_folds.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
     df = df.sample(frac=1).reset_index(drop=True)
     y = df.target.values
     kf = model_selection.StratifiedKFold(n_splits=10)
-    for fold_, (_, _) in enumerate(kf.split(X=df, y=y)):
-        df.loc[:, "kfold"] = fold_
+    for fold_, (t_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, "kfold"] = fold_
     df.to_csv(os.path.join(input_path, "train_folds.csv"), index=False)
     
